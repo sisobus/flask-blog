55dc5a3f45f37ed68f7e9688831f8145100e94de
root <root@ip-172-31-11-102.ap-northeast-2.compute.internal>
2016-07-05 08:44:55 +0000

    blog post add

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -23,6 +23,10 @@ from flask.ext.mail import Mail
 mail = Mail(app)
 from flask.ext.mail import Message
 from decorators import async
+import datetime
+import os
+import commands
+import glob
 
 @async
 def send_async_email(app, msg):
@@ -56,20 +60,77 @@ def contact_mail():
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    posts = get_all_posts()
+    ret_posts = get_all_post_information(posts)
+    return render_template('blog.html',ret_posts=ret_posts)
 
-@app.route('/a')
-def a():
-    return render_template('a.html')
+@app.route('/create_post')
+def create_post():
+    posts = get_all_posts()
+    ret_posts = get_all_post_information(posts)
+    return render_template('create_post.html',ret_posts=ret_posts)
 
-@app.route('/b')
-def b():
-    with open('visit','r') as fp:
-        count = int(fp.read().strip())
-    count += 1
-    with open('visit','w') as fp:
-        fp.write(str(count))
-    return render_template('b.html',count=count)
+def createDirectory(directoryName):
+    if not os.path.exists(directoryName):
+        command = 'mkdir %s'%directoryName
+        ret = commands.getoutput(command)
+        command = 'chmod 777 %s'%directoryName
+        ret = commands.getoutput(command)
+
+def get_max_post_number(filenames):
+    ret = 0
+    for filename in filenames:
+        only_filename = filename.split('/')[-1] 
+        only_number = int(only_filename.split('.')[0])
+        ret = max(ret, only_number)
+    return ret
+
+def get_all_posts():
+    createDirectory('/var/www/flask_blog/flask_blog/post/')
+    filenames = glob.glob('/var/www/flask_blog/flask_blog/post/*.post')
+    return filenames
+
+def get_all_post_information(post_names):
+    ret = []
+    for post_name in post_names:
+        d = {}
+        with open(post_name,'r') as fp:
+            lines = fp.read().strip().split('\n')
+        d['post_id'] = post_name.split('/')[-1].split('.')[0]
+        d['post_name'] = lines[0]
+        d['post_author'] = lines[1]
+        d['post_date'] = lines[2]
+        d['post_body'] = ''
+        for body in lines[3:]:
+            d['post_body'] += body
+        ret.append(d)
+    return ret
+
+@app.route('/save_post',methods=['POST'])
+def save_post():
+    if request.method == 'POST':
+        post_name = request.form['post_name']
+        post_author = request.form['post_author']
+        post_body = request.form['post_body']
+        post_date = datetime.datetime.now()
+        s = '%s\n%s\n%s\n%s'%(post_name,post_author,str(post_date),post_body)
+        print post_name, post_author, post_body, post_date
+        post_names = get_all_posts()
+        last_post_number = get_max_post_number(post_names)
+        next_filename = '/var/www/flask_blog/flask_blog/post/%d.post'%(last_post_number+1)
+        with open(next_filename,'w') as fp:
+            fp.write(s)
+        return redirect(url_for('main'))
+
+@app.route('/post/<post_id>')
+def post_detail(post_id):
+    posts = get_all_posts()
+    ret_posts = get_all_post_information(posts)
+    ret = {}
+    for post in ret_posts:
+        if post['post_id'].strip() == post_id.strip():
+            ret = post
+    return render_template('post.html',ret_posts=ret_posts,post=ret)
 
 if __name__ == '__main__':
     app.run(host='172.31.11.102',debug=True)
